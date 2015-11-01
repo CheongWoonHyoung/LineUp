76cc956129222d33c11ff209e264765ce4ef9c9b
JeonghyunLee <ljh3091426@naver.com>
2015-11-02 00:09:52 +0900

    rest_info  & mypage

--- LineUp/app/src/main/java/com/unist/am/lineup/Rest_info_tab1.java
+++ LineUp/app/src/main/java/com/unist/am/lineup/Rest_info_tab1.java
@@ -4,11 +4,15 @@ package com.unist.am.lineup;
  * Created by mark_mac on 2015. 10. 30..
  */
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class Rest_info_tab1 extends BaseFragment {
@@ -24,15 +28,42 @@ public class Rest_info_tab1 extends BaseFragment {
     Double y_coordinate = null;
     String phone_num = null;
     String dummyname;
+    Context parent_context;
 
-    public static Rest_info_tab1 newInstance(){
+    RelativeLayout go_to_map_btn;
+    Button menu;
+    public static Rest_info_tab1 newInstance(Context context){
         final Rest_info_tab1 fragment = new Rest_info_tab1();
+        fragment.parent_context = context;
         return fragment;
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.res_info_tab1, parent, false);
         mScrollView = findView(view, R.id.scroll_view_tab01);
+
+        go_to_map_btn = (RelativeLayout) view.findViewById(R.id.go_to_map);
+        menu = (Button) view.findViewById(R.id.menu);
+
+        go_to_map_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map_intent = new Intent(parent_context, MapActivity.class);
+                map_intent.putExtra("flag",true);
+                map_intent.putExtra("lat",y_coordinate);
+                map_intent.putExtra("lon",x_coordinate);
+                startActivityForResult(map_intent,1);
+            }
+        });
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
         return view;
     }
 
