af53345c72a0a284c722b5a651deedbeed4ff028
Cheong WoonHyoung <cheongwh@GidiaMacBook.local>
2015-11-03 01:01:16 +0900

    design

--- LineUp/app/src/main/java/com/unist/am/lineup/MyPageActivity.java
+++ LineUp/app/src/main/java/com/unist/am/lineup/MyPageActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -27,8 +28,8 @@ import ru.noties.scrollable.ScrollableLayout;
 public class MyPageActivity extends BaseActivity_myPage {
     private static final String ARG_LAST_SCROLL_Y = "arg.LastScrollY";
     private ScrollableLayout mScrollableLayout;
-    Button settingBtn;
-    Button BackBtn;
+    LinearLayout settingBtn;
+    LinearLayout BackBtn;
 
     String nickName;
     String profileImgURL;
@@ -45,7 +46,7 @@ public class MyPageActivity extends BaseActivity_myPage {
         profileImgURL=profile_data.getExtras().getString("profileImgURL");
         cus_name = (TextView) findViewById(R.id.profile_name);
         cus_profile = (ImageView) findViewById(R.id.profile);
-        cus_name.setText(nickName);
+        //cus_name.setText(nickName);
         //Picasso.with(this).load(profileImgURL).resize(120,120).into(cus_profile);
         final View header = findViewById(R.id.my_page_header);
         final TabsLayout_myPage tabs = findView(R.id.mypage_tabs);
@@ -97,8 +98,8 @@ public class MyPageActivity extends BaseActivity_myPage {
         }
 
         // header 부분
-        settingBtn = (Button) header.findViewById(R.id.settingBtn);
-        BackBtn = (Button) header.findViewById(R.id.backBtn);
+        settingBtn = (LinearLayout) header.findViewById(R.id.settingBtn);
+        BackBtn = (LinearLayout) header.findViewById(R.id.backBtn);
         settingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
